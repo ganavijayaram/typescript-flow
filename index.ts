9679f9642f60d1e0a00ccbd08933b505d137f730
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-04 10:53:27 -0400

    Using typescript to catch errors while coding

--- index.ts
+++ index.ts
@@ -4,5 +4,10 @@ const url = "https://jsonplaceholder.typicode.com/todos/1"
 
 //this is a asyn command and hence we know it will return promise and we need to handle it
 axios.get(url).then(response => {
-  console.log(response.data)
+  const {id, title, completed} = response.data
+  
+  console.log(`
+  Id: ${id} 
+  Title: ${title}
+  Completed: ${completed}`)
 })
